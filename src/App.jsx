86ee52ca30ed2f2b207f96ae94c065794522379f
abhay0480sharma <abhaykumar@Abhays-MacBook-Air.local>
2024-08-17 19:43:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+
+  const [menu,setMenu] = useState()
+  useEffect(() => {
+    (async () => {
+      try {
+        const halwaiMenuReq = await axios.get("api/partyMenu");
+        console.log("halwaiMenuReq", halwaiMenuReq);
+        setMenu(halwaiMenuReq?.data?.SOUPS)
+      } catch (error) {
+        console.error("Error fetching the Halwai menu:", error);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+     <div>
+      {menu?.map((items,index)=>{
+        return(
+          <div key={index}>{items}</div>
+        )
+      })}
+     </div>
+    </>
+  )
+}
+
+export default App
